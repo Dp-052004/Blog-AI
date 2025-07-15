0d5a2f8a87de18a615152269c4e985ff2e6947c7
Dp-052004 <121806338+Dp-052004@users.noreply.github.com>
2025-07-15 21:10:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,9 +136,7 @@ def spellcheck_word():
         return jsonify({"error": "Internal server error"}), 500
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
+
 
 
 
